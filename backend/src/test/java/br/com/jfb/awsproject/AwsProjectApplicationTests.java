7efd9ccd2351f9a77ac96491e77c7a7ef2be613a
josivaldobatista <josivaldo-fb@outlook.com>
2022-04-18 13:36:02 -0300

    Creation of the API

---
+++ backend/src/test/java/br/com/jfb/awsproject/AwsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jfb.awsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
